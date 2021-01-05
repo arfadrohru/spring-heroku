4cc63591482d7e886bdc6ae2ff38ba1af2f3e5b5
arfadrohru <arfadrohru@gmail.com>
2021-01-05 09:04:58 +0700

    initial commit

---
+++ src/main/java/com/deploy/heroku/controller/HerokuController.java
@@ -0,0 +1,20 @@
+package com.deploy.heroku.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HerokuController {
+
+	@GetMapping("/User")
+	public Map<String, Object> getUser(){
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("user", "Heroku");
+		return result;
+	}
+}
---
+++ src/test/java/com/deploy/heroku/HerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
